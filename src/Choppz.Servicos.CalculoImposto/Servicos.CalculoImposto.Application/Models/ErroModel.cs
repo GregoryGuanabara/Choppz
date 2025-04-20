5eafdae2ec4bc63ece2b7f644523cfe49d13520b
Gregory Guanabara <Gregoryguanabara@gmail.com>
2025-04-20 19:12:26 -0300

    feat: implementa modelos para resposta padronizada de erros na API

---
+++ src/Choppz.Servicos.CalculoImposto/Servicos.CalculoImposto.Application/Models/ErroModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Servicos.CalculoImposto.Application.Models
+{
+    internal sealed record ErroModel
+    {
+        public ErroModel(string propriedade, string mensagem)
+        {
+            Propriedade = propriedade;
+            Mensagem = mensagem;
+        }
+
+        public string Propriedade { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
