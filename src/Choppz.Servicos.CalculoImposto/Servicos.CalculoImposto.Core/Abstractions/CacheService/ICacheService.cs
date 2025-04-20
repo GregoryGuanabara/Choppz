ffb08994a33fbe5cf7621631d670ba66a9c125b5
Gregory Guanabara <Gregoryguanabara@gmail.com>
2025-04-20 18:14:52 -0300

    feat: adiciona mecanismo de cache em memória

---
+++ src/Choppz.Servicos.CalculoImposto/Servicos.CalculoImposto.Core/Abstractions/CacheService/ICacheService.cs
@@ -0,0 +1,13 @@
+﻿namespace Servicos.CalculoImposto.Core.Abstractions.CacheService
+{
+    public interface ICacheService
+    {
+        T? Pegar<T>(string key);
+
+        void Inserir<T>(string key, T value, TimeSpan expiration);
+
+        void Remover(string key);
+
+        bool Existe(string key);
+    }
+}
---
+++ src/Choppz.Servicos.CalculoImposto/Servicos.CalculoImposto.Infra/CacheService/CacheService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Servicos.CalculoImposto.Core.Abstractions.CacheService;
+
+namespace Servicos.CalculoImposto.Infra.CacheService
+{
+    public sealed class CacheService : ICacheService
+    {
+        private readonly IMemoryCache _cache;
+
+        public CacheService(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+        public T? Pegar<T>(string key)
+        {
+            if (_cache.TryGetValue(key, out var value) && value is T typedValue)
+                return typedValue;
+
+            return default(T);
+        }
+
+        public void Inserir<T>(string key, T value, TimeSpan expiration)
+        {
+            var cacheOptions = new MemoryCacheEntryOptions()
+                .SetAbsoluteExpiration(expiration);
+
+            _cache.Set(key, value, cacheOptions);
+        }
+
+        public void Remover(string key)
+        {
+            _cache.Remove(key);
+        }
+
+        public bool Existe(string key)
+        {
+            return _cache.TryGetValue(key, out _);
+        }
+    }
+}
