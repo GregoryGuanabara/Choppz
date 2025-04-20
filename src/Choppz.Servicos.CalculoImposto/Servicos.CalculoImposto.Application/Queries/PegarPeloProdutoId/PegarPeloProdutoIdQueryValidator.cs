7bb872fd72d3abc1903af4148d0f4ba726f49b60
Gregory Guanabara <Gregoryguanabara@gmail.com>
2025-04-20 20:19:47 -0300

    feat(application): adiciona PegarPeloPedidoIdQuery com suporte às dependências necessárias

---
+++ src/Choppz.Servicos.CalculoImposto/Servicos.CalculoImposto.Application/Queries/PegarPeloProdutoId/PegarPeloProdutoIdQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Servicos.CalculoImposto.Application.Queries.PegarPeloProdutoId
+{
+    public class PegarPeloProdutoIdQueryValidator : AbstractValidator<PegarPeloPedidoIdQuery>
+    {
+        public PegarPeloProdutoIdQueryValidator()
+        {
+            RuleFor(x => x.PedidoId)
+                    .NotEmpty().WithMessage("O ID do pedido não pode ser vazio.")
+                    .GreaterThan(0).WithMessage("O ID do pedido deve ser um valor valido.");
+        }
+    }
+}
